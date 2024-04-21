2293af06253a56e4de83371d44cc6c58368cec78
rortegaez <raulor83@gmail.com>
2024-04-21 15:54:50 +0200

    beggining search

--- src/components/Prueba.jsx
+++ src/components/Prueba.jsx
@@ -1,27 +1,54 @@
 import { ALIMENTOS, COLOR } from "./alimentos/alimentos";
+import { useState } from "react";
 
-console.log(ALIMENTOS, "alimentos");
-export default function Prueba() {
+const Prueba = () => {
+  const [element, setElement] = useState("");
+
   const type = [];
-  const name = [];
+  const namer = [];
+
   ALIMENTOS.map((item) => {
-    name.push(item.Alimento);
+    namer.push({ ident: item.id, food: item.Alimento.toLowerCase() });
     if (type.includes(item.tipo)) {
       return null;
     } else {
       type.push(item.tipo);
     }
   });
-  const prueba1 = () => {
-    console.log("funciona");
+
+  console.log(type, "tipos");
+  console.log(namer, "alimentos");
+
+  const handleBusqueda = (event) => {
+    setElement(event.target.value.toLowerCase());
   };
-  console.log(type, "lacteos");
-  console.log(name, "alimentos");
+  const handleSearch = () => {
+    if (element.length === 0) {
+      return null;
+    }
+    console.log(element);
+    namer.map((item) => {
+      if (item.food === element) {
+        console.log("encontrado");
+      } else {
+        console.log("no encontrado");
+      }
+    });
+  };
+
   return (
     <>
-      <button className="haber" onClick={() => prueba1()}>
+      <textarea
+        name="buscador"
+        id="buscador"
+        value={element}
+        onChange={handleBusqueda}
+        className="bg-slate-500 w-200 h-100"
+      ></textarea>
+      <button className="rounded" onClick={handleSearch}>
         Funcionará
       </button>
     </>
   );
-}
+};
+export default Prueba;
