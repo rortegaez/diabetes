68de7ab58670e5484d69f74615e7da58262817a5
rortegaez <raulor83@gmail.com>
2024-04-22 17:08:27 +0200

    change in jsx

--- src/components/Prueba.jsx
+++ src/components/Prueba.jsx
@@ -3,37 +3,22 @@ import { useState } from "react";
 
 const Prueba = () => {
   const [element, setElement] = useState("");
-
-  const type = [];
-  const namer = [];
-
-  ALIMENTOS.map((item) => {
-    namer.push({ ident: item.id, food: item.Alimento.toLowerCase() });
-    if (type.includes(item.tipo)) {
-      return null;
-    } else {
-      type.push(item.tipo);
-    }
-  });
-
-  console.log(type, "tipos");
-  console.log(namer, "alimentos");
+  const [resultSearch, setResultSearch] = [];
 
   const handleBusqueda = (event) => {
     setElement(event.target.value.toLowerCase());
   };
+  console.log(element, "element");
+
   const handleSearch = () => {
     if (element.length === 0) {
-      return null;
+      return <p>Ingrese el alimento</p>;
+    } else {
+      const search = ALIMENTOS.filter((item) => {
+        item.Alimento.toLowerCase().includes(element);
+      });
+      setResultSearch(search);
     }
-    const resultSearch = namer.filter((item) => {
-      return item.food.includes(element)
-    })
-    console.log(resultSearch, 'search')
-    ALIMENTOS.map((item) => {
-      const resultTotal = item.id.includes(resultSearch.ident)
-      console.log(resultTotal, 'total')
-    })
   };
 
   return (
@@ -47,8 +32,7 @@ const Prueba = () => {
       ></textarea>
       <button className="rounded" onClick={handleSearch}>
         Funcionará
-      </button> 
-      <h1></h1>
+      </button>
     </>
   );
 };
