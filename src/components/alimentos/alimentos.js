fc1cb089d83de376edb37128c9ad010e30b2ebfe
rortegaez <raulor83@gmail.com>
2024-04-25 20:26:08 +0200

    second add food

--- src/components/alimentos/alimentos.js
+++ src/components/alimentos/alimentos.js
@@ -637,4 +637,673 @@ export const ALIMENTOS = [
       }
     ]
   },
+  {
+    id: 48,
+    tipo: "cerealesyderivados",
+    Alimento: "Guisantes congelados, frescos, de lata",
+    RacionGramos: 100,
+    IG: 35,
+    color: COLOR.verde,
+    ejemplo: [
+      {
+        nombre: "plato grande",
+        cantidadGramos: 600,
+        RacionHC: 6
+      },
+      {
+        nombre: "plato mediano",
+        cantidadGramos: 400,
+        RacionHC: 4
+      },
+      {
+        nombre: "guarnición",
+        cantidadGramos: 200,
+        RacionHC: 2
+      }
+    ]
+  },
+  {
+    id: 49,
+    tipo: "cerealesyderivados",
+    Alimento: "Gofio",
+    RacionGramos: 12,
+  },
+  {
+    id: 50,
+    tipo: "cerealesyderivados",
+    Alimento: "Harina de avena",
+    RacionGramos: 18,
+  },
+  {
+    id: 51,
+    tipo: "cerealesyderivados",
+    Alimento: "Harina de cebada o centeno",
+    RacionGramos: 17,
+    IG: 48,
+    color: COLOR.verde,
+    ejemplo: [
+      {
+        nombre: "cuchara sopera rasa",
+        cantidadGramos: 14,
+        RacionHC: 0.8
+      }
+    ]
+  },
+  {
+    id: 52,
+    tipo: "cerealesyderivados",
+    Alimento: "Harina de maiz",
+    RacionGramos: 15,
+    IG: 70,
+    color: COLOR.rojo,
+    ejemplo: [
+      {
+        nombre: "cuchara sopera rasa",
+        cantidadGramos: 14,
+        RacionHC: 1
+      }
+    ]
+  },
+  {
+    id: 53,
+    tipo: "cerealesyderivados",
+    Alimento: "Harina de soja",
+    RacionGramos: 70,
+    IG: 25,
+    color: COLOR.verde,
+    ejemplo: [
+      {
+        nombre: "cuchara sopera rasa",
+        cantidadGramos: 14,
+        RacionHC: 0.2
+      }
+    ]
+  },
+  {
+    id: 54,
+    tipo: "cerealesyderivados",
+    Alimento: "Harina de trigo",
+    RacionGramos: 15,
+    IG: 78,
+    color: COLOR.rojo,
+    ejemplo: [
+      {
+        nombre: "cuchara sopera rasa",
+        cantidadGramos: 14,
+        RacionHC: 1
+      }
+    ]
+  },
+  {
+    id: 55,
+    tipo: "cerealesyderivados",
+    Alimento: "Hojaldre crudo",
+    RacionGramos: 30
+  },
+  {
+    id: 56,
+    tipo: "cerealesyderivados",
+    Alimento: "Hojaldre horneado",
+    RacionGramos: 24,
+    IG: 56,
+    color: COLOR.naranja,
+  },
+  {
+    id: 57,
+    tipo: "cerealesyderivados",
+    Alimento: "Hummus",
+    RacionGramos: 100,
+  },{
+    id: 58,
+    tipo: "cerealesyderivados",
+    Alimento: "Judias blancas, crudo",
+    RacionGramos: 25
+  },
+  {
+    id: 59,
+    tipo: "cerealesyderivados",
+    Alimento: "Judias blancas, en corservas",
+    RacionGramos: 70,
+    IG: 35,
+  },
+  {
+    id: 60,
+    tipo: "cerealesyderivados",
+    Alimento: "Judias blancas, hervido",
+    RacionGramos: 55,
+    IG: 35,
+    color: COLOR.verde,
+    ejemplo: [
+      {
+        nombre: "plato grande",
+        cantidadGramos: 300,
+        RacionHC: 6
+      },
+      {
+        nombre: "plato mediano",
+        cantidadGramos: 200,
+        RacionHC: 6
+      },
+      {
+        nombre: "guarnicion",
+        cantidadGramos: 100,
+        RacionHC: 2
+      }
+    ]
+  },
+  {
+    id: 61,
+    tipo: "cerealesyderivados",
+    Alimento: "Legumbre, en conservas",
+    RacionGramos: 70
+  },
+  {
+    id: 62,
+    tipo: "cerealesyderivados",
+    Alimento: "Lentejas, crudo",
+    RacionGramos: 20
+  },
+  {
+    id: 63,
+    tipo: "cerealesyderivados",
+    Alimento: "Lentejas, en conservas",
+    RacionGramos: 70,
+    IG: 35
+  },
+  {
+    id: 64,
+    tipo: "cerealesyderivados",
+    Alimento: "Lentejas, hervido",
+    RacionGramos: 50,
+    IG: 35,
+    color: COLOR.verde,
+    ejemplo: [
+      {
+        nombre: "plato grande",
+        cantidadGramos: 300,
+        RacionHC: 6
+      },
+      {
+        nombre: "plato mediano",
+        cantidadGramos: 200,
+        RacionHC: 4
+      },
+      {
+        nombre: "guarnición",
+        cantidadGramos: 100,
+        RacionHC: 2
+      }
+    ]
+  },
+  {
+    id: 65,
+    tipo: "cerealesyderivados",
+    Alimento: "Maiz en lata",
+    RacionGramos: 50,
+    IG: 65,
+    color: COLOR.naranja,
+    ejemplo: [
+      {
+        nombre: "lata pequeña",
+        cantidadGramos: 150,
+        RacionHC: 3
+      }
+    ]
+  },
+  {
+    id: 66,
+    tipo: "cerealesyderivados",
+    Alimento: "Maiz en lata sin azucar añadido",
+    RacionGramos: 90,
+    ejemplo: [
+      {
+        nombre: "lata pequeña",
+        cantidadGramos: 150,
+        RacionHC: 1.5
+      }
+    ]
+  },
+  {
+    id: 67,
+    tipo: "cerealesyderivados",
+    Alimento: "Maiz tostado (quicos)",
+    RacionGramos: 20,
+    ejemplo: [
+      {
+        nombre: "bolsa pequeña",
+        cantidadGramos: 40,
+        RacionHC: 2
+      }
+    ]
+  },
+  {
+    id: 68,
+    tipo: "cerealesyderivados",
+    Alimento: "Mijo, crudo",
+    RacionGramos: 15,
+  },
+  {
+    id: 69,
+    tipo: "cerealesyderivados",
+    Alimento: "Mijo, cocido",
+    RacionGramos: 53,
+    IG: 70,
+    color: COLOR.rojo
+  },
+  {
+    id: 70,
+    tipo: "cerealesyderivados",
+    Alimento: "Plan blanco (de trigo)",
+    RacionGramos: 20,
+    IG: 70,
+    color: COLOR.rojo,
+    ejemplo: [
+      {
+        nombre: "Barra de 1/4 unos 3cm",
+        cantidadGramos: 20,
+        RacionHC: 1
+      },
+      {
+        nombre: "Barra de 1/4 entera",
+        cantidadGramos: 180,
+        RacionHC: 9
+      },
+      {
+        nombre: "Panecillo restaurante",
+        cantidadGramos: 60,
+        RacionHC: 3
+      }
+    ]
+  },
+  {
+    id: 71,
+    tipo: "cerealesyderivados",
+    Alimento: "Pan de centeno",
+    RacionGramos: 20,
+    IG: 65,
+    color: COLOR.naranja,
+  },
+  {
+    id: 72,
+    tipo: "cerealesyderivados",
+    Alimento: "Pan de hamburguesa o Frankfurt",
+    RacionGramos: 18,
+    IG: 85,
+    color: COLOR.rojo,
+    ejemplo: [
+      {
+        nombre: "unidad",
+        cantidadGramos: 80,
+        RacionHC: 4.5
+      },
+      {
+        nombre: "unidad pequeña",
+        cantidadGramos: 55,
+        RacionHC: 3
+      }
+    ]
+  },
+  {
+    id: 73,
+    tipo: "cerealesyderivados",
+    Alimento: "Pan de molde",
+    RacionGramos: 20,
+    IG: 85,
+    color: COLOR.rojo,
+    ejemplo: [
+      {
+        nombre: "rebanada",
+        cantidadGramos: 25,
+        RacionHC: 1.2
+      }
+    ]
+  },
+  {
+    id: 74,
+    tipo: "cerealesyderivados",
+    Alimento: "Pan integral",
+    RacionGramos: 23,
+    IG: 40,
+    color: COLOR.verde,
+  },
+  {
+    id: 75,
+    tipo: "cerealesyderivados",
+    Alimento: "Pan rallado",
+    RacionGramos: 15,
+    IG: 70,
+    color: COLOR.rojo,
+    ejemplo: [
+      {
+        nombre: "cuchara sopera colmada",
+        cantidadGramos: 23,
+        RacionHC: 1.5
+      }
+    ]
+  },
+  {
+    id: 76,
+    tipo: "cerealesyderivados",
+    Alimento: "Pan tostado o biscote",
+    RacionGramos: 15,
+    IG: 70,
+    color: COLOR.rojo,
+    ejemplo: [
+      {
+        nombre: "unidad",
+        cantidadGramos: 10,
+        RacionHC: 0.6
+      },
+      {
+        nombre: "unidad mini",
+        cantidadGramos: 3.3,
+        RacionHC: 0.25
+      },
+      {
+        nombre: "tostada canape",
+        cantidadGramos: 2,
+        RacionHC: 0.15
+      },
+      {
+        nombre: "krispoll",
+        cantidadGramos: 12.5,
+        RacionHC: 0.8
+      },
+      {
+        nombre: "palitos 3 palitos",
+        cantidadGramos: 45,
+        RacionHC: 1
+      }
+    ]
+  },
+  {
+    id: 77,
+    tipo: "cerealesyderivados",
+    Alimento: "Pasta alimenticia, crudo",
+    RacionGramos: 14,
+  },
+  {
+    id: 78,
+    tipo: "cerealesyderivados",
+    Alimento: "Pasta alimenticia, hervido",
+    RacionGramos: 45,
+    IG: 50,
+    color: COLOR.verde,
+    ejemplo: [
+      {
+        nombre: "plato grande",
+        cantidadGramos: 270,
+        RacionHC: 6
+      },
+      {
+        nombre: "plato mediano",
+        cantidadGramos: 180,
+        RacionHC: 4
+      },
+      {
+        nombre: "guarnicion",
+        cantidadGramos: 90,
+        RacionHC: 2
+      }
+    ]
+  },
+  {
+    id: 79,
+    tipo: "cerealesyderivados",
+    Alimento: "Pasta alimenticia integral, crudo",
+    RacionGramos: 15,
+  },
+  {
+    id: 80,
+    tipo: "cerealesyderivados",
+    Alimento: "Pasta alimenticia integral, hervido",
+    RacionGramos: 45,
+    IG: 45,
+    color: COLOR.verde,
+    ejemplo: [
+      {
+        nombre: "plato grande",
+        cantidadGramos: 270,
+        RacionHC: 6
+      },
+      {
+        nombre: "plato mediano",
+        cantidadGramos: 180,
+        RacionHC: 4
+      },
+      {
+        nombre: "guarnicion",
+        cantidadGramos: 90,
+        RacionHC: 2
+      }
+    ]
+  },
+  {
+    id: 81,
+    tipo: "cerealesyderivados",
+    Alimento: "Pasta al huevo, crudo",
+    RacionGramos: 16,
+  },
+  {
+    id: 82,
+    tipo: "cerealesyderivados",
+    Alimento: "Pasta fresca rellena",
+    RacionGramos: 30
+  },
+  {
+    id: 83,
+    tipo: "cerealesyderivados",
+    Alimento: "Pasta sin gluten",
+    RacionGramos: 13,
+    IG: 73,
+    color: COLOR.rojo,
+  },
+  {
+    id: 84,
+    tipo: "cerealesyderivados",
+    Alimento: "Patata, crudo",
+    RacionGramos: 65,
+  },
+  {
+    id: 85,
+    tipo: "cerealesyderivados",
+    Alimento: "Patata hervida",
+    RacionGramos: 65,
+    IG: 65,
+    color: COLOR.naranja,
+    ejemplo: [
+      {
+        nombre: "plato grande",
+        cantidadGramos: 300,
+        RacionHC: 5
+      },
+      {
+        nombre: "plato mediano",
+        cantidadGramos: 200,
+        RacionHC: 3
+      },
+      {
+        nombre: "guarnicion",
+        cantidadGramos: 100,
+        RacionHC: 1.5
+      }
+    ]
+  },
+  {
+    id: 86,
+    tipo: "cerealesyderivados",
+    Alimento: "Patata, al horno o asada",
+    RacionGramos: 35,
+    IG: 62,
+    color: COLOR.naranja,
+  },
+  {
+    id: 87,
+    tipo: "cerealesyderivados",
+    Alimento: "Patatas fritas",
+    RacionGramos: 30,
+    IG: 70,
+    color: COLOR.rojo,
+    ejemplo: [
+      {
+        nombre: "guarnicion",
+        cantidadGramos: 60,
+        RacionHC: 2
+      },
+      {
+        nombre: "porcion hamburgueseria",
+        cantidadGramos: 120,
+        RacionHC: 4
+      }
+    ]
+  },
+  {
+    id: 88,
+    tipo: "cerealesyderivados",
+    Alimento: "Patatas chips",
+    RacionGramos: 20,
+    IG: 95,
+    color: COLOR.rojo,
+    ejemplo: [
+      {
+        nombre: "bolsa pequeña",
+        cantidadGramos: 30,
+        RacionHC: 1.5
+      }
+    ]
+  },
+  {
+    id: 89,
+    tipo: "cerealesyderivados",
+    Alimento: "Pure de patatas, en copos",
+    RacionGramos: 15,
+    IG: 90,
+    color: COLOR.rojo,
+  },
+  {
+    id: 90,
+    tipo: "cerealesyderivados",
+    Alimento: "Pure de patata, elaborado con leche",
+    RacionGramos: 80,
+    IG: 90,
+    color: COLOR.rojo,
+  },
+  {
+    id: 91,
+    tipo: "cerealesyderivados",
+    Alimento: "Quinoa, crudo",
+    RacionGramos: 19,
+  },
+  {
+    id: 92,
+    tipo: "cerealesyderivados",
+    Alimento: "Quinoa, hervido",
+    RacionGramos: 48,
+    IG: 35,
+    color: COLOR.verde,
+  },
+  {
+    id: 93,
+    tipo: "cerealesyderivados",
+    Alimento: "Semola de trigo, crudo",
+    RacionGramos: 14,
+  },
+  {
+    id: 94,
+    tipo: "cerealesyderivados",
+    Alimento: "Semola de trigo, hervido",
+    RacionGramos: 90,
+    IG: 67,
+    color: COLOR.naranja,
+  },
+  {
+    id: 95,
+    tipo: "cerealesyderivados",
+    Alimento: "Soja seca, crudo",
+    RacionGramos: 30,
+  },
+  {
+    id: 96,
+    tipo: "cerealesyderivados",
+    Alimento: "Soja seca, hervido",
+    RacionGramos: 100,
+    IG: 15,
+    color: COLOR.verde,
+  },
+  {
+    id: 97,
+    tipo: "cerealesyderivados",
+    Alimento: "Sushi",
+    RacionGramos: 45,
+    IG: 42,
+    color: COLOR.verde,
+    ejemplo: [
+      {
+        nombre: "pieza pequeña",
+        cantidadGramos: 20,
+        RacionHC: 0.5
+      },
+      {
+        nombre: "pieza grande",
+        cantidadGramos: 40,
+        RacionHC: 0.9
+      }
+    ]
+  },
+  {
+    id: 98,
+    tipo: "cerealesyderivados",
+    Alimento: "Tapioca, crudo",
+    RacionGramos: 12,
+  },
+  {
+    id: 99,
+    tipo: "cerealesyderivados",
+    Alimento: "Tapioca, hervido",
+    RacionGramos: 33,
+    IG: 84,
+    color: COLOR.rojo,
+  },
+  {
+    id: 100,
+    tipo: "cerealesyderivados",
+    Alimento: "Trigo sarraceno, crudo",
+    RacionGramos: 14,
+  },
+  {
+    id: 101,
+    tipo: "cerealesyderivados",
+    Alimento: "Trigo sarraceno, hervido",
+    RacionGramos: 42,
+    IG: 40,
+    color: COLOR.verde,
+  },
+  {
+    id: 102,
+    tipo: "cerealesyderivados",
+    Alimento: "Trigo tierno, crudo",
+    RacionGramos: 16,
+  },
+  {
+    id: 103,
+    tipo: "cerealesyderivados",
+    Alimento: "Trigo tierno, hervido",
+    RacionGramos: 39,
+    IG: 54,
+    color: COLOR.verde,
+  },
+  {
+    id: 104,
+    tipo: "cerealesyderivados",
+    Alimento: "Yuca, crudo",
+    RacionGramos: 25,
+  },
+  {
+    id: 105,
+    tipo: "cerealesyderivados",
+    Alimento: "Yuca, cocido",
+    RacionGramos: 33,
+    IG: 55,
+    color: COLOR.verde,
+  },
 ]
