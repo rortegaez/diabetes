ba902eeb781bc410fd85afeb8978c529b9c893e2
rortegaez <raulor83@gmail.com>
2025-01-17 15:01:03 +0100

    fix form

--- src/components/element/SearchFood.jsx
+++ src/components/element/SearchFood.jsx
@@ -1,33 +1,11 @@
-import { useEffect, useState } from "react";
-import { FOOD } from "../food/FOOD.js";
+import { useFood } from "../hooks/useFood";
 
 export const SearchFood = () => {
-  const [searchFood, setSearchFood] = useState("");
-  const [error, setError] = useState(null);
-
-  const getFood = ({ searchFood }) => {
-    console.log(searchFood, "searchoooo");
-    if (searchFood === " ") return null;
-
-    const searchChange = searchFood
-      .toLowerCase()
-      .normalize("NFD")
-      .replace(/[\u0300-\u036f]/g, "");
-
-    FOOD.forEach((item) => {
-      const alimentoChange = item.Alimento.toLowerCase()
-        .normalize("NFD")
-        .replace(/[\u0300-\u036f]/g, "");
-      if (alimentoChange.includes(searchChange)) {
-        console.log(item, "item");
-      }
-    });
-  };
+  const { getFood, searchFood, setSearchFood } = useFood();
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log(searchFood, "submit");
-    getFood({ searchFood });
+    getFood();
   };
 
   const handleSearch = (event) => {
@@ -36,24 +14,13 @@ export const SearchFood = () => {
     setSearchFood(newQuery);
   };
 
-  useEffect(() => {
-    if (searchFood === "") {
-      setError("No se puede hacer una busqueda vacia");
-      return;
-    }
-    if (searchFood.match(/^\d+$/)) {
-      setError("No se puede realizar una buscqueda por número");
-      return;
-    }
-  }, [SearchFood]);
-
   return (
     <>
       <form onSubmit={handleSubmit}>
         <input
           name="search"
           onChange={handleSearch}
-          value={searchFood}
+          value={searchFood?.value}
           placeholder="Pizza, arroz, patatas..."
         />
         <button type="submit">Buscar</button>
---
+++ src/components/hooks/useFood.js
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react";
+import { FOOD } from "../food/FOOD";
+
+export function useFood () {
+	const [searchFood, setSearchFood] = useState('')
+	const [error, setError] = useState(null)
+
+	const getFood = () => {
+			console.log(searchFood, "searchoooo");
+			if (searchFood === " ") return null;
+
+			const searchChange = searchFood
+				.toLowerCase()
+				.normalize("NFD")
+				.replace(/[\u0300-\u036f]/g, "");
+
+			FOOD.forEach((item) => {
+				const alimentoChange = item.Alimento.toLowerCase()
+					.normalize("NFD")
+					.replace(/[\u0300-\u036f]/g, "");
+				if (alimentoChange.includes(searchChange)) {
+					console.log(item, "item");
+				}
+			});
+		};
+
+		useEffect(() => {
+			if (searchFood === "") {
+				setError("No se puede hacer una busqueda vacia");
+				return;
+			}
+			if (searchFood?.match(/^\d+$/)) {
+				setError("No se  =puede realizar una buscqueda por número");
+				return;
+			}
+		}, [searchFood]);
+
+		return({getFood, error, setSearchFood})
+}
