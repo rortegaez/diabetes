dcb22b0d7d8f29f922f9fab31ef06042ea7558d0
rortegaez <raulor83@gmail.com>
2025-01-20 12:34:46 +0100

    fix ButtonClick

--- src/components/component/ButtonClick.jsx
+++ src/components/component/ButtonClick.jsx
@@ -1,17 +1,10 @@
 import PropTypes from "prop-types";
 
-export const ButtonClick = ({ type, name, onClick, props }) => {
+export const ButtonClick = ({ type, name, onClick }) => {
   return (
     <>
       <button type={type} onClick={onClick}>
         {name}
-        {props?.map((itemFood) => (
-          <div key={itemFood.nombre}>
-            <p>{itemFood.nombre}</p>
-            <p>{itemFood.cantidadGramos}</p>
-            <p>{itemFood.RacionHC}</p>
-          </div>
-        ))}
       </button>
     </>
   );
@@ -21,5 +14,4 @@ ButtonClick.propTypes = {
   type: PropTypes.string,
   name: PropTypes.string,
   onClick: PropTypes.func,
-  props: PropTypes.array,
 };
